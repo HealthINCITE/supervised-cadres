15a4891583ab701109b844422f10ab884d05454a
Jason Kuruzovich <jkuruzovich@gmail.com>
2019-06-04 12:56:59 -0400

    removed clone from install script.

--- build_tools/travis/install_script.sh
+++ build_tools/travis/install_script.sh
@@ -2,4 +2,5 @@ pip install -r requirements.txt
 export WORKING_DIR=`pwd`
 echo "> Working dir: $WORKING_DIR"
 echo "> Getting Supervised GMM..."
-git clone https://github.com/jorje1908/SGMM
+#git clone https://github.com/jorje1908/SGMM
+#added as submodule.
