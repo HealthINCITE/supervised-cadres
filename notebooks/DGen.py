d5a8df9d9fdeac984cb8716baf36a148315fb02a
fiifi57 <fiifi57@users.noreply.github.com>
2019-06-05 17:10:06 -0400

    dgen_module

---
+++ notebooks/DGen.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed June 05 17:07 2019
+
+@authors: jkuruzovich, karhin
+"""
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from sklearn.datasets.samples_generator import make_blobs
+from  sklearn.datasets import make_classification
+from  sklearn.datasets import make_regression
+from matplotlib import pyplot
+import pandas as pd    
+import numpy as np
+
+def generate_cadres(samples, features, informative, cadres, seed):
+    """
+    samples = sample size
+    features = cadre features 
+    cadres = number of cadres
+    informative = random state
+    seed = random state
+    """
+    samples_per_cadre=int(samples/cadres)
+    X, y= make_classification(n_samples=samples, n_features=features, n_informative=informative,  n_classes=cadres,  random_state=seed)
+    c_columns = ['cad'+str(x) for x in range (features)] #['cad'+str(x) for x in range (cf)]
+    df = pd.DataFrame(X, columns = c_columns)
+    df["cadre"] = y
+    df=df.sort_values(by=['cadre'], )
+    df=df.reset_index(drop=True)
+    df['index_c']= [x for y in range(cadres) for x in range(samples_per_cadre)]
+    return df
+
+def generate_variables(df, samples, features, classes, informative, seed):
+        # df = dataframe
+        # features = number of features
+        # n = sample size
+        # c = cadre you wish to generate variables for
+        # r = random state
+        # y = target / dependent variable / number of responses for categorical variable
+        # X = independent variables
+        df2=pd.DataFrame()
+        
+        for cadre in range(cadres):
+            seed_c=cadre*seed
+            X, y= make_classification(n_samples=samples, n_features=features, n_informative=informative,  n_classes=classes,  random_state=seed_c)
+            columns= [ 'dv'+str(x) for x in range(features)]
+            df3 = pd.DataFrame(X, columns = columns)
+            df3["target"]=y
+            df3["cadre"]=cadre
+            df3["index_c"]=df3.index
+            df2=df2.append(df3)
+            #df2.merge(df3,left_on='cadre',right_on='cadre')
+        #df2=pd.concat([df2.loc[df2.cadre==0,], df3], axis=1, join='inner')
+        return df2
